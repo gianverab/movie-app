8c82f62517ebdf683452b0d8a6ccdcf52d71f558
Giancarlo <gianverab@gmail.com>
2025-04-23 00:28:24 -0500

    setup screen and tab navigation

---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Saved = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-5xl text-dark-200 font-bold">Saved</Text>
+    </View>
+  );
+};
+
+export default Saved;
+
+const styles = StyleSheet.create({});
---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Search = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-5xl text-dark-200 font-bold">Search</Text>
+    </View>
+  );
+};
+
+export default Search;
+
+const styles = StyleSheet.create({});
